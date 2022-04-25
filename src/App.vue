7dd92262de9dd4b1da8bb3b5dfe8073dc79ec56d
nazareth Astudillo <nazastudillo@gmail.com>
2022-04-25 13:15:37 -0700

    weather app base

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,32 @@
   <div class="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search...">
+        <input type="text" class="search-bar" placeholder="Search..." />
       </div>
     </main>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
-      api_key: '9f5340571f4c6c4f374064135c1a72f9'
-    }
-  }
-
-  }
+      api_key: "9f5340571f4c6c4f374064135c1a72f9",
+    };
+  },
+};
 </script>
 
 <style>
 * {
-margin: 0;
-padding: 0;
-box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 body {
-  font-family: 'montserrat', sans-serif;
+  font-family: "montserrat", sans-serif;
 }
 
 #app {
